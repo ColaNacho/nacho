8ccfcbd58e29cd552a6c2054d72ffc4fa8b63188
colaNacho <aa01066114967@gmail.com>
2023-02-17 01:05:49 +0900

    두 정수 사이의 합 구하기

---
+++ src/codingTest01/Solution.java
@@ -0,0 +1,21 @@
+package codingTest01;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a==b){
+            answer=a;
+            return answer;
+        }else if(a>b){
+            for(int i=b; i<=a; i++){
+                answer+=i;
+            }
+            return answer;
+        }else if(b>a){
+            for(int i=a; i<=b; i++){
+                answer+=i;
+            }return answer;
+        }
+        return answer;
+    }
+}
